e920b8bf797e6bc4dad3e2bdfe6d969456f37292
TestSheepNZ <mike_talks@hotmail.com>
2016-08-25 21:18:02 +1200

    Link

--- JAVA6-BasicMethod/src/basicMethod.java
+++ JAVA6-BasicMethod/src/basicMethod.java
@@ -1,6 +1,6 @@
 /*
  * This program is part of my learning Java series
- * This one looks at use of methods
+ * This one looks at use of methods 
  * 
  * Find the blog here,
  * http://testsheepnz.blogspot.co.nz/2016/08/java-6-avoiding-tangled-code-with.html
