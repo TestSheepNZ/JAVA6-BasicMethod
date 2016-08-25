687ee850e4b3c01dd74c8d99457ab85cd4955d6f
TestSheepNZ <mike_talks@hotmail.com>
2016-08-25 16:06:04 +1200

    Done

---
+++ JAVA6-BasicMethod/src/basicMethod.java
@@ -0,0 +1,15 @@
+
+public class basicMethod {
+
+	private static void printMe()
+	{
+		System.out.println("Hello, World");
+	}
+	
+	public static void main(String[] args) {
+
+		// Call the method to print
+		printMe();
+	}
+
+}
